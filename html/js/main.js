48ab62d2655967d95b0fb6bdd8f9bf1190928e26
codboxer <naieemsupto@gmail.com>
2016-09-18 02:16:09 +0600

    final update from home

--- html/js/main.js
+++ html/js/main.js
@@ -6,7 +6,7 @@
  *################# 
  */
 $(document).ready(function () {
-    
+
     /*########## accordion toggle icon function ##########*/
     function toggleIcon(e) {
         $(e.target)
@@ -17,18 +17,35 @@ $(document).ready(function () {
     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
     $('.panel-group').on('shown.bs.collapse', toggleIcon);
     /*####### end accordion panel ############*/
-    
+
     $(".nav-pills li:first-child").addClass("active");
 
     $('#dg-container').gallery();
     $('.navbar-fixed-top').affix({
         offset: {
-            top: 100
+            top: 20
         }
     });
-    $(".pointer a").click(function(e){
+    $(".pointer a").click(function (e) {
         e.preventDefault();
     });
+
+    var $scrollingDiv = $("#animatedDiv");
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 600) {
+            $scrollingDiv.stop().animate({"marginTop": ($(window).scrollTop() + 150) + "px"}, "slow");
+        }
+        else{
+            $scrollingDiv.stop().animate({"marginTop":"600px"}, "slow");
+        }
+    });
+    $(".cross").click(function(e){
+        e.preventDefault();
+        $("#animatedDiv").hide('fast');
+    });
+
+
 });
 
 
